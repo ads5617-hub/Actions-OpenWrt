06da7abae6d5b74fb1e7d8273945604b24dee3f0
ads5617-hub <hxzyyz@gmail.com>
2026-02-04 09:41:18 +0800

    Update diy-part2.sh to install feeds and clone repos
    Updated the script to install all feeds and clone necessary repositories.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+./scripts/feeds update -a
+./scripts/feeds install -a
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
-
-# ===== daed & luci-app-daed =====
 git clone https://github.com/daeuniverse/dae.git package/dae
 git clone https://github.com/daeuniverse/daed.git package/daed
 git clone https://github.com/QiuSimons/luci-app-daed.git package/luci-app-daed
-
