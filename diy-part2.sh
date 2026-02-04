ce23f8eb86ec979d544bfa9f860a4e3f4415725a
ads5617-hub <hxzyyz@gmail.com>
2026-02-04 20:19:54 +0800

    Update diy-part2.sh to clone daed packages
    Removed feed update and install commands, added cloning of daed packages.

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-./scripts/feeds update -a
-./scripts/feeds install -a
-
+# 拉 daed 相关包（在 feeds install 之前）
 git clone https://github.com/daeuniverse/dae.git package/dae
 git clone https://github.com/daeuniverse/daed.git package/daed
 git clone https://github.com/QiuSimons/luci-app-daed.git package/luci-app-daed
